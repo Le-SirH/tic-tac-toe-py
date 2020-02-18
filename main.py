c2b9d7912f86b1f85b6f429b9afa6c6cb766f085
Ryan Harrington <rharrington.web@gmail.com>
2020-02-18 08:39:17 -0700

    board & move function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Henlo
+# Numbers from 0 to 8, tracking the positions of the board.
+# Each number will be replaced with an 'X' or 'O' eventually.
+board = [n for n in range(9)]
+
+# Tracks the past moves of each player, preventing them from making the same move
+xMoves = []
+oMoves = []
+
+# Whenever a move is made, this modifies the board data & tracks past moves.
+def move(player, pos):
+    if player == 'X':
+        xMoves.append(pos)
+    board[pos] = player
+    return print(board)
