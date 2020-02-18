6c26e48d243e3e3ebf3c8475963b3a0a0ab16288
Ryan <rwait8831@westada.org>
2020-02-18 09:08:32 -0700

    main.py coin flip

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ board = [n for n in range(9)]
 # Tracks the past moves of each player, preventing them from making the same move
 xMoves = []
 oMoves = []
-
+choice=""
 # Whenever a move is made, this modifies the board data & tracks past moves.
 def move(player, pos):
     if player == 'X':
@@ -19,3 +19,15 @@ def move(player, pos):
 
 # Initializes the AI
 bot = bot.Bot('X')
+# Randomly picks heads or tails to see who takes the first move
+def coinFlip(choice):
+    choice=random.choice(["heads","tails"])
+x0=random.choice(["x","o"])
+print("Welcome to TicTacToe, you are",x0, + "'s. To see who goes first we will flip a coin.")
+coinFlip()
+userChoice=input("Heads or Tails")
+if userChoice==choice:
+    print("It was",choice, "You will go first, make your first move.")
+    
+else:
+    print("It was",choice,"I will go first. Good luck beating me >:| ")    
