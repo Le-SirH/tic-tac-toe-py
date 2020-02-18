f9dcd20377cecbfd32ee5110a3a05ccfc2f7f3c0
Ryan Harrington <rharrington.web@gmail.com>
2020-02-18 09:54:27 -0700

    Game class & game.start

--- main.py
+++ main.py
@@ -1,38 +1,67 @@
 import bot
+import random
 
-# Numbers from 0 to 8, tracking the positions of the board.
-# Each number will be replaced with an 'X' or 'O' eventually.
-board = [n for n in range(9)]
+class Game:
 
-# Tracks the past moves of each player, preventing them from making the same move
-xMoves = []
-oMoves = []
-choice = random.choice(["heads","tails"])
-players = ["X","O"]
-playerChoice = random.randint(1, 2)
-player = players[player]
+    def __init__(self, player, bot, firstMove):
+        # Numbers from 0 to 8, tracking the positions of the board.
+        # Each number will be replaced with an 'X' or 'O' eventually.
+        self.board = [n for n in range(9)]
+        self.player = player
+        self.firstMove = firstMove
+        self.bot = bot
 
-# Whenever a move is made, this modifies the board data & tracks past moves.
-def move(player, pos):
-    if player == 'X':
-        xMoves.append(pos)
-    else:
-        oMoves.append(pos)
-    board[pos] = player
-    return print(board)
+    def start(self):
 
-# Initializes the AI
-bot = bot.Bot()
+        if self.player == self.firstMove:
+            usrMove = input("Choose your first move: ")
+            self.move(self.player, usrMove)
+
+        while self.movesLeft() > 0:
+            
+            self.move(self.bot.player, self.bot.chooseMove(self.board))
+            self.checkWinner()
+            self.printBoard()
+            
+            usrMove = input("Choose a move: ")
+            while self.isTaken(usrMove):
+                print("That move is taken!")
+                usrMove = input("Choose a move: ")
+            
+            self.move(self.player, usrMove)
+            self.checkWinner()
+
+    def move(self, player, pos):
+        self.board[int(pos)] = player
+        return print(self.board)
+
+    def movesLeft(self):
+        return len([pos for pos in self.board if type(pos) != str])
+
+    def checkWinner(self):
+        pass
+    
+    def isTaken(self, movePos):
+        pass
+
+    def printBoard(self):
+        pass
 
 # Randomly picks heads or tails to see who takes the first move
+choice = random.choice(["heads", "tails"])
 
-
-print("Welcome to TicTacToe, you are", player, "'s. To see who goes first we will flip a coin.")
-
-userChoice=input("Heads or Tails")
+print("Welcome to TicTacToe. To see who goes first we will flip a coin.")
+userChoice = input("Heads or Tails: ")
+print("It was {}.".format(choice))
 
 if userChoice.lower() == choice:
-    print("It was", choice , "You will go first, make your first move.")
+    bot = bot.Bot('O')
+    game = Game('X', bot, 'X')
+    print("You will go first, make your first move.")
 
 else:
-    print("It was",choice,"I will go first. Good luck beating me >:| ")
+    bot = bot.Bot('X')
+    game = Game('O', bot, bot.player)
+    print("I will go first. Good luck beating me >:D")
+
+game.start()
