d546bab7b498f39a28a90d27471d03b4872278d0
Ryan H <46948579+Le-SirH@users.noreply.github.com>
2020-02-23 19:58:55 -0700

    Update bot.py
    Added descriptions of the chooseMove() function.=

--- bot.py
+++ bot.py
@@ -13,17 +13,25 @@ class Bot:
         self.winPosGoal = random.choice(winningPos)
 
     def chooseMove(self, board):
+        # filters the board positions to tell which are available
         positions = [pos for pos in self.board if type(pos) != str]
         for pos in winningPos:
+            # filters the moves available for each winning position
             pMoves = [pMove for pMove in pos if type(self.board[pMove]) == str]
+            # determines the positions of the available winning moves
             winningMovesLeft = [p for p in pos if type(self.board[p]) != str]
+            # determines whether there is a spot to be blocked
             if (len(pMoves) == 2) & (len(winningMovesLeft) == 1):
                 if self.board[pMoves[0]] == self.board[pMoves[1]]:
                     return winningMovesLeft[0]
+        
+        # If there are no blocks to be made, make a move based on a winning position.
+        # winPosGoal is a randomly chosen winning position that the bot goes for.
         winMove = random.choice(self.winPosGoal)
         if self.isTaken(winMove) == False:
             return winMove
         else: 
+            # resets winPosGoal & chooses a random position on the board.
             self.winPosGoal = random.choice(winningPos)
             return random.choice(positions)
 
